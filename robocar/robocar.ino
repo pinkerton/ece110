491ca544b4814a625843c8123d0c0d05d778a4a8
Stephen Pinkerton <stephenmpinkerton@gmail.com>
2015-04-15 15:19:33 -0500

    Add debugging for photo sensor

--- robocar/robocar.ino
+++ robocar/robocar.ino
@@ -6,6 +6,9 @@ https://learn.adafruit.com/adafruit-arduino-lesson-13-dc-motors/arduino-code
 
 flex sensor code:
 https://learn.sparkfun.com/tutorials/sik-experiment-guide-for-arduino---v32/experiment-9-using-a-flex-sensor
+
+photo sensor code:
+https://learn.adafruit.com/photocells/using-a-photocell
 */
 
 // ANALOG IN pins
@@ -19,6 +22,9 @@ const int rightMotor = 6;
 // must be >= 0 && <= 255
 int leftSpeed = 255;
 int rightSpeed = 255;
+
+int leftLightValue;
+int rightLightValue;
  
 void setup() 
 { 
@@ -37,11 +43,30 @@ void loop()
   leftLightValue = analogRead(leftLight);
   rightLightValue = analogRead(rightLight);
   
-  Serial.print("left light sensor: ");
+  /*Serial.print("left light sensor: ");
   Serial.println(leftLightValue);
   
   Serial.print("right light sensor: ");
-  Serial.println(rightLightValue);
+  Serial.println(rightLightValue);*/
+  
+  
+  // print how bright the left photo sensor thinks it is
+  
+  Serial.print("Analog reading = ");
+  Serial.print(leftLightValue);     // the raw analog reading
+ 
+  // We'll have a few threshholds, qualitatively determined
+  if (leftLightValue < 10) {
+    Serial.println(" - Dark");
+  } else if (leftLightValue < 200) {
+    Serial.println(" - Dim");
+  } else if (leftLightValue < 500) {
+    Serial.println(" - Light");
+  } else if (leftLightValue < 800) {
+    Serial.println(" - Bright");
+  } else {
+    Serial.println(" - Very bright");
+  }
   
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
