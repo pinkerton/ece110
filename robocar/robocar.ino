9010cd08b054b941d860b850e9a317920711b161
Rohan Mathur <rohan@rmathur.com>
2015-04-22 18:39:32 -0500

    Clean up code for report
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- robocar/robocar.ino
+++ robocar/robocar.ino
@@ -1,20 +1,18 @@
-/* 
-info about the board: https://learn.sparkfun.com/tutorials/redboard-hookup-guide
+/*
+    info about the board: https://learn.sparkfun.com/tutorials/redboard-hookup-guide
 
-motor control code taken on 4/15/2015 from
-https://learn.adafruit.com/adafruit-arduino-lesson-13-dc-motors/arduino-code
+    motor control code taken on 4/15/2015 from
+    https://learn.adafruit.com/adafruit-arduino-lesson-13-dc-motors/arduino-code
 
-flex sensor code:
-https://learn.sparkfun.com/tutorials/sik-experiment-guide-for-arduino---v32/experiment-9-using-a-flex-sensor
+    flex sensor code:
+    https://learn.sparkfun.com/tutorials/sik-experiment-guide-for-arduino---v32/experiment-9-using-a-flex-sensor
 
-photo sensor code:
-https://learn.adafruit.com/photocells/using-a-photocell
+    photo sensor code:
+    https://learn.adafruit.com/photocells/using-a-photocell
 */
 
 // ANALOG IN pins
-const int leftLightSensor = 2;
-//const int rightLightSensor = 1;
-
+const int lightSensor = 2;
 
 // ANALOG OUT pins
 const int leftMotor = 5;
@@ -25,44 +23,33 @@ const int speaker = 8;
 int leftSpeed = 55;
 int rightSpeed = 55;
 
-int leftLightValue;
-int rightLightValue;
+int lightValue;
 
 const int THRESHOLD = 910;
 
 int freq = 440;
 int duration = 1000;
 
-void setup() 
-{ 
+void setup()
+{
   pinMode(leftMotor, OUTPUT);
   pinMode(rightMotor, OUTPUT);
   // initialize serial communications at 9600 bps
   Serial.begin(9600);
-} 
- 
- 
-void loop() 
-{ 
+}
+
+
+void loop()
+{
   analogWrite(leftMotor, leftSpeed);
   analogWrite(rightMotor, rightSpeed);
-  
-  leftLightValue = analogRead(leftLightSensor);
-  //rightLightValue = analogRead(rightLightSensor);
-  
-  /*Serial.print("left light sensor: ");
-  Serial.println(leftLightValue);
-  
-  Serial.print("right light sensor: ");
-  Serial.println(rightLightValue);*/
-  
-  
-  // print how bright the left photo sensor thinks it is
-  
+
+  lightValue = analogRead(lightSensor);
+
   //Serial.print("Analog reading = ");
-  //Serial.print(leftLightValue);     // the raw analog reading
-  
-  if (leftLightValue > THRESHOLD) {
+  Serial.print(lightValue);     // the raw analog reading
+
+  if (lightValue > THRESHOLD) {
     analogWrite(leftMotor, leftSpeed);
     analogWrite(rightMotor, rightSpeed);
   } else {
@@ -70,26 +57,9 @@ void loop()
     analogWrite(rightMotor, 0);
     tone(speaker, freq, 2);
   }
- 
-  // We'll have a few threshholds, qualitatively determined
-  
-  /*
-  Serial.print(leftLightValue);
-  if (leftLightValue < 10) {
-    Serial.println(" - Dark");
-  } else if (leftLightValue < 200) {
-    Serial.println(" - Dim");
-  } else if (leftLightValue < 500) {
-    Serial.println(" - Light");
-  } else if (leftLightValue < 800) {
-    Serial.println(" - Bright");
-  } else {
-    Serial.println(" - Very bright");
-  }*/
 
-  
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
-  // after the last reading. necessary??
+  // after the last reading.
   delay(2);
 }
